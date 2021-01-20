bf4849d91efe46c82de3d4a6b4db0f3e9a7b7a33
gsi-plank <a92960@alunos.uminho.pt>
2021-01-20 12:36:34 +0000

    criar form

--- Frontend/controller/newAuditor.js
+++ Frontend/controller/newAuditor.js
@@ -1,24 +1,17 @@
-window.onload = function() {
-    const urlBase = "https://58717807e0f449edb5fcb157313592f1.vfs.cloud9.us-east-1.amazonaws.com";
+import * as fetch from "./functions/fetch.js";
 
-// validator();
-//     document.getElementById("formNewUser").onsubmit = function(e) {
-//         //validação do formulário ao submeter
-//         validator();
-//     };
+
+    window.onload = function(){
+    validator();
     
     //submit
-    document.getElementById("formNewUser").submit();
+    // document.getElementById("formNewUser").onsubmit = function(e) {
+    //     //validação do formulário ao submeter
+    //     validator();
+    // };
     
-    const formNewUser = document.getElementById("formNewUser");
-    document.querySelector(".submit").addEventListener("click", function(){
+  
 
-  formNewUser.submit();
-
-});
-    
-    
-    
     //função de validação
     function validator() {
         new Validator(document.querySelector('form[name="formNewUser"]'), function(err, res) {
@@ -45,6 +38,7 @@ window.onload = function() {
     
 function saveUsers() {
         var data = {};
+        let route = "users/audits";
         data.id_auditor = document.getElementById("id_auditor").value;
         data.id_login = document.getElementById("id_login").value;
         data.name = document.getElementById("name").value;
@@ -53,40 +47,8 @@ function saveUsers() {
         data.password = document.getElementById("pwd").value;
         console.log(data); //debugging para ver os dados que foram enviados
         //chamada fetch para envio dos dados para o servior via POST
-        fetch(`${urlBase}/users/audits`, {
-            headers: { 'Content-Type': 'application/json' },
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
-        .then(function(response) {
-            if (!response.ok) {
-                console.log(response.status); //=> number 100–599
-                console.log(response.statusText); //=> String
-                console.log(response.headers); //=> Headers
-                console.log(response.url); //=> String
-                
-                if (response.status === 409) {
-                    alert("Duplicated Email");
-                }
-                
-                else {
-                    throw Error(response.statusText);
-                }
-            }
-            
-            else {
-                document.getElementById("formNewUser").reset(); //limpeza dos dados do form
-                alert("submitted with success");
-            }
-            
-        })
-        .then(function(result) {
-            console.log(result);
-        })
-        .catch(function(err) {
-            alert("Submission error");
-            console.error(err);
-        });
+        
+        fetch.postData(route, data);
     }
-    
+
 }
