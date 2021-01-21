c290460e855c51eef991e5509e982dd0cf012939
gsi-plank <a92960@alunos.uminho.pt>
2021-01-21 00:12:01 +0000

    Revert "alterações be"
    This reverts commit 6a5d731fa950dcd9d3a8302d299cb2ea5bcea31e.

--- webbook/routes/operational.route.js
+++ webbook/routes/operational.route.js
@@ -3,11 +3,11 @@ const controllerIndiv = require('../controllers/individual_evaluation.controller
 const controllerOperational = require('../controllers/operational.controller')
 const bodyParser = require('body-parser');
 
-server.route('/operationals/totalpoints')
+server.route('/operational/totalpoints')
     .get(controllerIndiv.listOpPointsTotal)
 
-server.route('/operationals')
+server.route('/operational')
     .get(controllerOperational.listOperational)
 
-server.route('/operationals/:id_operational')
+server.route('/operational/:id_operational')
     .get(controllerOperational.readOperational)
