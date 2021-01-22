169b0c2f0398d27d151572469fab169689175e7c
gsi-plank <a92960@alunos.uminho.pt>
2021-01-22 16:17:01 +0000

    fff

--- Frontend/controller/login.js
+++ Frontend/controller/login.js
@@ -1,30 +1,18 @@
 
 
- window.onload = function() {
-     
-     let view = document.getElementById("togglePassword");
-     view.onclick = viewButton;
-
-    let login = document.getElementById("submitInput");
-    login.onclick = mainPage;
-    
-    function mainPage() {
-        window.location.href = "mainPage.html";
-    }
-     //Fuction of the "view" button
-     function viewButton() {
-         let view = document.getElementById("passInput");
+    document.getElementById("togglePassword").addEventListener("click", function() {
+        let view = document.getElementById("passInput");
          if (view.type === "password") {
              view.type = "text";
          }
          else {
              view.type = "password";
          }
-     }
- };
+    })
+
 
 //   SPLASH
-    setTimeout(() => {
-          document.getElementById('splash').classList.toggle('fade');
-      },2000);
+    // setTimeout(() => {
+    //       document.getElementById('splash').classList.toggle('fade');
+    //   },2000);
  
