5143609edcc1a6bbc47de3c1caa25759c73847f5
gsi-plank <a92960@alunos.uminho.pt>
2021-01-23 22:52:31 +0000

    front com mais fetch

--- Frontend/controller/externelReview.js
+++ Frontend/controller/externelReview.js
@@ -9,11 +9,11 @@ import * as fetch from "./functions/fetch.js"
 // });~
 
 $('#myTable').on('click', 'i', function () {
-    $(this).closest('')remove();
+    $(this).closest('div').remove();
 })
 
 $('p button').click(function () {
-    $('#myTable').append('<tr><td><h4>Testemunho</h4><input type="text" class="fname" /></td><td><input class="remBtn" id="remBtn" type="button" value="Remover"></tr><tr><td><h4>Justificação</h4><input type="text" class="fname" /></td><td><input class="remBtn" id="remBtn" type="button" value="Remover"></tr>')
+    $('#myTable').append('<div class="field"><div class="input-wrapper"><h4>Testemunho:</h4><input type="text" class="teste"><i class="fas fa-times"></i></input></div></div><div class="field"><div class="input-wrapper"><h4>Justificação:</h4><input type="text" class="teste"/><i class="fas fa-times"></i></input></div></div>')
 });
 
 
