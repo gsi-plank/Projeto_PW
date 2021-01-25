f41b93c2effc2dd90c46aff4ec11447fd8414f4d
gsi-plank <a92960@alunos.uminho.pt>
2021-01-25 12:24:06 +0000

    2

--- webbook/server.js
+++ webbook/server.js
@@ -22,33 +22,33 @@ server.listen(port, function(err) {
 });
 
 //multer
-const path = require ('path');  
-app.use('/uploads', express.static(path.join(__dirname, '/upload')));  
-const storage = multer.diskStorage({ 
-    destination: (req, file, cb) => { 
-        cb(null, 'uploads'); 
-    },  
-    filename: (req, file, cb) => { 
-        console.log(file); 
-        let name = Date.now() + path.extname(file.originalname); 
-        cb(null, name); 
-        let nameUpload = "__ROTA___" + name);  
-        localStorage.setItem("localUploadedFileName", nameUpload); //gravar a variável nameUpload, para depois enviar para a BD a rota da foto 
-    }  
-}); 
+// const path = require ('path');  
+// app.use('/uploads', express.static(path.join(__dirname, '/upload')));  
+// const storage = multer.diskStorage({ 
+//     destination: (req, file, cb) => { 
+//         cb(null, 'uploads'); 
+//     },  
+//     filename: (req, file, cb) => { 
+//         console.log(file); 
+//         let name = Date.now() + path.extname(file.originalname); 
+//         cb(null, name); 
+//         let nameUpload = "__ROTA___" + name);  
+//         localStorage.setItem("localUploadedFileName", nameUpload); //gravar a variável nameUpload, para depois enviar para a BD a rota da foto 
+//     }  
+// }); 
 
-const fileFilter = (req, file, cb) => {  
-    if (file.mimetype == 'image/jpeg' || file.mimetype == +image/png){  
-        cb(null, true); 
-    } else {  
-        cb(null, false); 
-    }
-} 
+// const fileFilter = (req, file, cb) => {  
+//     if (file.mimetype == 'image/jpeg' || file.mimetype == +image/png){  
+//         cb(null, true); 
+//     } else {  
+//         cb(null, false); 
+//     }
+// } 
 
-const upload = multer({  storage: storage, fileFilter: fileFilter }); 
-app.post('/upload', upload.single('image'), (req, res, next) => {  
-    res.sendFile('Upload.html', { root: '_______'});   
-}); 
+// const upload = multer({  storage: storage, fileFilter: fileFilter }); 
+// app.post('/upload', upload.single('image'), (req, res, next) => {  
+//     res.sendFile('Upload.html', { root: '_______'});   
+// }); 
 
 //forçar utilização das bibliotecas
 server.use(cors());
